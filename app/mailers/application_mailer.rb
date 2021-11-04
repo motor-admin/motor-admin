3344908c17ccef6293cf11d71ed662d0eada0eb8
Pete Matsyburka <pete.matsy@gmail.com>
2021-11-04 15:19:11 +0200

    fix alerts sending default email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'reports@getmotoradmin.com'
   layout 'mailer'
 end
