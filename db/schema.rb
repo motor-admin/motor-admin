8522b09ce4dee3ff2ae63eecffc714891a2d6386
Pete Matsyburka <pete.matsy@gmail.com>
2021-12-05 18:54:30 +0200

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_081429) do
+ActiveRecord::Schema.define(version: 2021_11_24_011025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "motor_admin_user_roles", force: :cascade do |t|
     t.bigint "admin_user_id", null: false
@@ -190,6 +218,8 @@ ActiveRecord::Schema.define(version: 2021_08_05_081429) do
     t.index ["name"], name: "motor_tags_name_unique_index", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "motor_admin_user_roles", "motor_admin_users", column: "admin_user_id"
   add_foreign_key "motor_admin_user_roles", "motor_roles", column: "role_id"
   add_foreign_key "motor_alert_locks", "motor_alerts", column: "alert_id"
