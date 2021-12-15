4ee0cfea4921e9c041640d2f1a03ee6d4cd9944b
Pete Matsyburka <pete.matsy@gmail.com>
2021-12-15 12:56:57 +0200

    add docker build image

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,6 @@ SECRET_KEY_BASE=`openssl rand -hex 50` RAILS_ENV=production rake assets:precompi
 
 ../ruby-packer/bin/rubyc ./bin/motor-admin
 
-curl --upload-file a.out transfer.sh/$(uname)_$(uname -m)
-
 echo
 
 git checkout Gemfile Gemfile.lock
