be11d0c8e4e72ff5feb9573854d504d2fd671620
Pete Matsyburka <pete.matsy@gmail.com>
2022-02-15 23:43:37 +0200

    fix rubocop

--- config/initializers/net_http.rb
+++ config/initializers/net_http.rb
@@ -3,5 +3,5 @@
 File.write('/tmp/motor-admin.pem', File.read(NetHttpSslFix::LOCAL_CA_FILE))
 
 Kernel.silence_warnings do
-  NetHttpSslFix.const_set('LOCAL_CA_FILE', '/tmp/motor-admin.pem')
+  NetHttpSslFix.const_set(:LOCAL_CA_FILE, '/tmp/motor-admin.pem')
 end
