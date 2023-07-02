ba7ba42897d805607d951c6f3e327a781485a757
Pete Matsyburka <pete.matsy@gmail.com>
2023-07-02 13:09:57 +0300

    iframe ALLOWALL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+  config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
   config.active_storage.variant_processor = :mini_magick
